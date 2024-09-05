49c9a8979cdd2c8bebdb103d7d6ca4aa491afa25
Ishika Patel <ishika.patel@sportzinteractive.com>
2024-09-05 13:47:17 +0530

    firstCommit

---
+++ SI_chessBackend/DAO/IChess.cs
@@ -0,0 +1,16 @@
+﻿using SI_chess.Models;
+
+namespace SI_chess.DAO
+{
+    public interface IChess
+    {
+        Task<int> InsertMatches(Matches m);
+        Task<Matches> GetMatchesById(int id);
+
+        Task<List<Players>> GetPlayersByCountry(string country);
+
+        Task<List<PlayerPerformance>> GetPlayerPerformance();
+
+        Task<List<PlayerPerformance>> GetPlayerWithHighestWon();
+    }
+}
---
+++ SI_chessBackend/Models/PlayerPerformance.cs
@@ -0,0 +1,10 @@
+﻿namespace SI_chess.Models
+{
+    public class PlayerPerformance
+    {
+        public string Name { get; set; }
+        public int TotalMatchesPlayed { get; set; }
+        public int TotalWinCount { get; set; }
+        public Double WinPercentage { get; set; }
+    }
+}
